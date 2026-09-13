86fb9c4c42cb40bda99070686a2c7e32fbdd4229
Anurag Rawat <anuragrwat7878@gmail.com>
2026-09-13 15:58:35 +0530

    Add program to find the second largest number

---
+++ DAY35 CODE69.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, a[100], largest, second;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    largest = second = -99999;
+
+    for(int i = 0; i < n; i++) {
+        if(a[i] > largest) {
+            second = largest;
+            largest = a[i];
+        }
+        else if(a[i] > second && a[i] != largest) {
+            second = a[i];
+        }
+    }
+
+    printf("%d", second);
+
+    return 0;
+}
