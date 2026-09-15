04da6bf17839d739a24b33d8f2ed5b6500896230
Anurag Rawat <anuragrwat7878@gmail.com>
2026-09-15 19:57:30 +0530

    Implement matrix row sum calculation

---
+++ DAY37 CODE73.C
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, c, i, j;
+    int a[100][100], sum[100];
+
+    scanf("%d %d", &r, &c);
+
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++)
+    {
+        sum[i] = 0;
+
+        for(j = 0; j < c; j++)
+        {
+            sum[i] = sum[i] + a[i][j];
+        }
+    }
+
+    for(i = 0; i < r; i++)
+    {
+        printf("%d ", sum[i]);
+    }
+
+    return 0;
+}
