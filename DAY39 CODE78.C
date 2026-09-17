951ee740750e2a4cfce93e4688638b895fe48971
Anurag Rawat <anuragrwat7878@gmail.com>
2026-09-17 15:19:43 +0530

    Add matrix diagonal sum calculation
    Implement a program to calculate the sum of the main diagonal of a matrix.

---
+++ DAY39 CODE78.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int r, c, a[10][10], i, j, sum = 0;
+
+    scanf("%d %d", &r, &c);
+
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++) {
+        sum = sum + a[i][i];
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
